3ff6fba21fe40a44899374f4839a62e5ee774da4
tdmpajak <tdm.pajak10@gmail.com>
2026-07-21 21:34:07 +0700

    Update TRACKER_API_URL to new Google Apps Script URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // URL Web App Google Apps Script -- HARUS SAMA PERSIS dengan yang dipakai
 // NC Tracker (lihat file assets/config.js di project NC Tracker), karena
 // NC Verifier "menumpang" ke backend yang sama (satu Google Sheet & Drive).
-const TRACKER_API_URL = "PASTE_URL_WEB_APP_YANG_SAMA_DENGAN_NC_TRACKER";
+const TRACKER_API_URL = "https://script.google.com/macros/s/AKfycbzobKrrnIIktNv1wLoqp6DopSYQM3ua581U7esOvoD-Vl43awmze0tHegzh7DTe6jRUYQ/exec";
