9716523790bf912b3902836fa7d788ceafd5d9e1
tdmpajak <tdm.pajak10@gmail.com>
2026-07-21 21:25:28 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// URL Web App Google Apps Script -- HARUS SAMA PERSIS dengan yang dipakai
+// NC Tracker (lihat file assets/config.js di project NC Tracker), karena
+// NC Verifier "menumpang" ke backend yang sama (satu Google Sheet & Drive).
+const TRACKER_API_URL = "PASTE_URL_WEB_APP_YANG_SAMA_DENGAN_NC_TRACKER";
